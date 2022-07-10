c4a84d44d0c739a526c7dcaf0daf5436e03dbb25
Mohammed <mohammed8cherfaoui@gmail.com>
2022-07-10 21:17:02 +0100

    fix: client-side exception in `/my/listings/` page
    I'm not a clown, I'm the entire circus.
    I should've checked if `userHasAgency` before doing a `map` on the array

--- pages/my/listings.js
+++ pages/my/listings.js
@@ -1,5 +1,6 @@
 import { EyeIcon } from '@heroicons/react/outline';
 import { Grid } from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
 import useSWR from 'swr';
 
 import { FlexText, HeroIcon } from '@/components/GlobalComponents';
@@ -10,30 +11,34 @@ import NoAgencyWarning from '@/components/NoAgencyWarning';
 import { fetcher } from '@/utils/fetcher';
 
 export default function MyListingsPage() {
-  const { data: userListings, isLoading } = useSWR(
-    '/api/user/listings',
+  const { data: session, status } = useSession();
+  const isLoadingSession = status === 'loading';
+  const userHasAgency = session?.user?.agencyId;
+  const { data: userListings } = useSWR(
+    userHasAgency ? '/api/user/listings' : '',
     fetcher
   );
   return (
     <MyPagesLayout pageTitle='My Listings'>
-      {userListings?.map((listing) => (
-        <Grid key={listing.id} xs={10} sm={4}>
-          <Grid.Container>
-            <Grid xs={12}>
-              <FlexText>
-                <HeroIcon>
-                  <EyeIcon />
-                </HeroIcon>
-                {listing?.views?.totalViews ?? 'No views'}
-              </FlexText>
-            </Grid>
-            <Grid xs={12}>
-              <ListingCard listing={listing} />
-            </Grid>
-          </Grid.Container>
-        </Grid>
-      ))}
-      {!userListings?.length && !isLoading && (
+      {userHasAgency &&
+        userListings?.map((listing) => (
+          <Grid key={listing.id} xs={10} sm={4}>
+            <Grid.Container>
+              <Grid xs={12}>
+                <FlexText>
+                  <HeroIcon>
+                    <EyeIcon />
+                  </HeroIcon>
+                  {listing?.views?.totalViews ?? 'No views'}
+                </FlexText>
+              </Grid>
+              <Grid xs={12}>
+                <ListingCard listing={listing} />
+              </Grid>
+            </Grid.Container>
+          </Grid>
+        ))}
+      {!userHasAgency && !isLoadingSession && (
         <Grid xs={12}>
           <NoAgencyWarning />
         </Grid>
